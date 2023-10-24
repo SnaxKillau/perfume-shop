3ab6f1e1032ddb29248911775d725f28f1b8e528
Versna <phopversna007@gmail.com>
2023-10-24 14:30:46 +0700

    product from api

---
+++ app/src/main/java/kh/edu/rupp/ite/perfume_shop/api/service/ProductApiService.kt
@@ -0,0 +1,10 @@
+package kh.edu.rupp.ite.perfume_shop.api.service
+
+import kh.edu.rupp.ite.perfume_shop.api.model.Product
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface ProductApiService {
+    @GET("/product")
+    fun loadProductList(): Call<List<Product>>
+}
